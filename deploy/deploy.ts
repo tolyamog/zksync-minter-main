49827da3169cf686680b80f1edc6b836c876ff6e
tolyamog <79326390+tolyamog@users.noreply.github.com>
2022-09-30 16:40:51 +0500

    Update deploy.ts

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -17,13 +17,13 @@ export default async function (hre: HardhatRuntimeEnvironment) {
   const artifact = await deployer.loadArtifact("Thistle");
 
   // // Deposit some funds to L2
-  // const depositAmount = ethers.utils.parseEther("0.002");
-  // const depositHandle = await deployer.zkWallet.deposit({
-  //   to: deployer.zkWallet.address,
-  //   token: utils.ETH_ADDRESS,
-  //   amount: depositAmount,
-  // });
-  // await depositHandle.wait();
+   const depositAmount = ethers.utils.parseEther("0.002");
+   const depositHandle = await deployer.zkWallet.deposit({
+     to: deployer.zkWallet.address,
+     token: utils.ETH_ADDRESS,
+     amount: depositAmount,
+   });
+   await depositHandle.wait();
 
   function getAccessToken() {
     console.log("✅ getAccessToken");
