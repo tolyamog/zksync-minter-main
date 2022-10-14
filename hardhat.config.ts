ecafda3dfef08b25f48d4811f49bb4dce4107803
tolyamog <79326390+tolyamog@users.noreply.github.com>
2022-10-14 21:43:45 +0500

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,32 +1,30 @@
-require("@matterlabs/hardhat-zksync-deploy");
-require("@matterlabs/hardhat-zksync-solc");
-import * as dotenv from 'dotenv';
-dotenv.config();
+require('@matterlabs/hardhat-zksync-deploy')
+require('@matterlabs/hardhat-zksync-solc')
 
 module.exports = {
   zksolc: {
-    version: "1.2.0",
-    compilerSource: "docker",
+    version: '1.2.0',
+    compilerSource: 'docker',
     settings: {
       optimizer: {
         enabled: true,
       },
       experimental: {
-        dockerImage: "matterlabs/zksolc",
+        dockerImage: 'matterlabs/zksolc',
+        tag: 'v1.2.0',
       },
     },
   },
   zkSyncDeploy: {
-    zkSyncNetwork: "https://zksync2-testnet.zksync.dev",
-    ethNetwork: "https://goerli.infura.io/v3/"+process.env.INFURA_PROJECT_ID,
+    zkSyncNetwork: 'https://zksync2-testnet.zksync.dev',
+    ethNetwork: 'goerli', // Can also be the RPC URL of the network (e.g. `https://goerli.infura.io/v3/<API_KEY>`)
   },
   networks: {
-    // To compile with zksolc, this must be the default network.
     hardhat: {
       zksync: true,
     },
   },
   solidity: {
-    version: "0.8.16",
+    version: '0.8.16',
   },
-};
+}
